22901132182f285b34abec160e4e13b2515979de
afatc <alexandrefilipecampos@gmail.com>
2014-05-19 15:24:08 +0100

    testing

--- src/main/java/com/yahoo/omid/tso/TSOHandler.java
+++ src/main/java/com/yahoo/omid/tso/TSOHandler.java
@@ -327,6 +327,7 @@ public class TSOHandler extends SimpleChannelHandler {
 
             if (reply.committed) {
                 // 2. commit
+            	LOG.warn(" ********************************* commit check");
                 try {
                     long commitTimestamp = timestampOracle.next(toWAL);
                     sharedState.uncommited.commit(commitTimestamp);
